d37a0df392aa69712efec492ceb01a94ede77943
venkatesh <venkateshdiddi3@gmail.com>
2021-12-24 22:31:19 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// we have created one constant date object
+const currentDate = new Date();
+console.log(currentDate);
+const newdate = new Date(2022,3,25,7,32,52);
+console.log(newdate);
+console.log(newdate-currentDate);
